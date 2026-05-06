6ffb7d764b7e069a65b8784ea2af1a94ca3e73b4
aneurokumar <aneurokumar@gmail.com>
2026-05-06 18:30:26 +0200

    fix content config system

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,40 +1,33 @@
 import { defineCollection, z } from "astro:content";
 
-const shared = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    tags: z.array(z.string()).optional(),
-    created: z.string().option(),
-    "last-modified": z.string().optional(),
-    type: z.string().optional(),
-    public: z.boolean().optional(),
-  }),
-});
+const shared = {
+  title: z.string().optional(),
+  description: z.string().optional(),
+  tags: z.array(z.string()).optional(),
+  created: z.string().optional(),
+  "last-modified": z.string().optional(),
+  type: z.string().optional(),
+  public: z.boolean().optional(),
+};
 
 const blog = defineCollection({
   type: "content",
-  schema: z.object({...shared}),
-  }),
+  schema: z.object({ ...shared }),
 });
 
 const work = defineCollection({
   type: "content",
-  schema: z.object({...shared}),
-  }),
+  schema: z.object({ ...shared }),
 });
 
 const projects = defineCollection({
   type: "content",
-  schema: z.object({...shared}),
-  }),
+  schema: z.object({ ...shared }),
 });
 
 const brain = defineCollection({
   type: "content",
-  schema: z.object({shared}),
-  }),
+  schema: z.object({ ...shared }),
 });
 
-export const collections = { work, projects, blog, brain };
+export const collections = { blog, work, projects, brain };
